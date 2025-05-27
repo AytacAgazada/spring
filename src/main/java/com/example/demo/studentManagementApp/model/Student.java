49e2f292e22f64e90f5aa932911d02851e168ca1
Aytac Agazade <aytacagazade1603@gmail.com>
2025-05-27 21:54:43 +0400

    feat: implement Student model with id, name, and age fields using Lombok and JPA

---
+++ src/main/java/com/example/demo/studentManagementApp/model/Student.java
@@ -0,0 +1,18 @@
+package com.example.demo.studentManagementApp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+
+}
